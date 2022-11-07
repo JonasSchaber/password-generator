8d498673ce7d95ac8248a0b5879f6115646ed1df
Jonas <62488564+JonasSchaber@users.noreply.github.com>
2022-11-07 18:36:19 +0100

    phyton

--- function.py
+++ function.py
@@ -1,8 +1,8 @@
 #Python-Code hier
 #print("Hallo Welt!")
 
-import secrets
-import string
+#import secrets
+#import string
 
-chars = string.digits + string.ascii_letters + string.punctuation
-print(''.join(secrets.choice(chars) for _ in range(40)))
+#chars = string.digits + string.ascii_letters + string.punctuation
+#print(''.join(secrets.choice(chars) for _ in range(40)))
