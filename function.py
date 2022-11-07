8296bafee529ac2441c87892be7a8b61402d7c8c
Jonas <62488564+JonasSchaber@users.noreply.github.com>
2022-11-07 18:15:56 +0100

    upload files
    upload files

---
+++ function.py
@@ -0,0 +1,8 @@
+#Python-Code hier
+#print("Hallo Welt!")
+
+import secrets
+import string
+
+chars = string.digits + string.ascii_letters + string.punctuation
+print(''.join(secrets.choice(chars) for _ in range(40)))
